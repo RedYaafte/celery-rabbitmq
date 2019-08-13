d201f6d4678d881bf3eb5560ea674588a6d168f7
Yafté M <redyafte@outlook.com>
2019-08-13 18:37:28 -0500

    View new messages in celery :eyes:

--- messenger/views.py
+++ messenger/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .tasks import count_messages
+from .models import Message
 
 
 def index(request):
     send_message = count_messages.delay()
     print("send message", send_message)
-    return HttpResponse("Hola mundo")
+    message = Message.objects.all()
+    return render(request, 'messenger/index.html', {'message': message})
