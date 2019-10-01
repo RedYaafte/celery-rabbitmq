94147684c1168a15ada3a33f3b2c90b50927b5c1
RedYaafte <redyafte@gmail.com>
2019-10-01 18:28:01 -0500

    Import messenger

--- messenger/apps.py
+++ messenger/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
 
 
-class MessengerConfig(AppConfig):
+class MessagesConfig(AppConfig):
     name = 'messenger'
+
+    def ready(self):
+        import messenger.signals
