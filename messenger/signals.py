bca6c82896e676f5386388ce139399f214c3df6e
RedYaafte <redyafte@gmail.com>
2019-10-03 18:39:19 -0500

    Added messenger tasks

--- messenger/signals.py
+++ messenger/signals.py
@@ -3,11 +3,28 @@ from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 
 from .models import Message
+from messenger.tasks import multiplicate
+from notifications.tasks import test_time, test_condition
+from notifications.models import ConfigurationNotification
 
 User = get_user_model()
 
 
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance=None, created=False, **kwargs):
-    print("pass", flush=True)
-    pass
+    """
+    Added type in task
+    """
+
+    if created:
+        event = ConfigurationNotification.objects.filter(
+            type=instance.event).values(
+                'pk', 'type', 'time', 'condition').first()
+        print("event ---->", event, flush=True)
+
+        if event['time']:
+            print("time", flush=True)
+            test_time.delay(event)
+        if event['condition']:
+            print("condition", flush=True)
+            test_condition.delay(event)
